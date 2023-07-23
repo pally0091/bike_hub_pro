ec2f6e00410876718869c174b245e2a54e378873
Debashis Roy <goodquote39@hotmail.com>
2023-07-23 18:36:02 +0600

    navbar 1 done

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,12 +1,22 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <div>
-            <p>Navbar</p>
-        </div>
-    );
+  return (
+    <div>
+      <div className="navbar bg-gradient-to-r from-slate-200 to-slate-700">
+        <Link to="/">
+          <button
+            id="nav-btn"
+            className=" normal-case border-4 border-double border-white px-4 rounded-3xl hover:bg-slate-500 transition-all duration-700"
+          >
+            <p id="nav-name">Bike Hub</p>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
